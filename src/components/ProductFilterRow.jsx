fade310aa055073b9c13aa1ba937b8ab11b28fb5
baharkyc <baharokuyucu@gmail.com>
2025-05-04 22:52:06 +0300

    Enhance ProductFilterRow to support sorting functionality and manage sort menu state

--- src/components/ProductFilterRow.jsx
+++ src/components/ProductFilterRow.jsx
@@ -4,9 +4,10 @@ import {ButtonSm} from "./buttons/ButtonSm";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
-const ProductFilterRow = ({ onViewChange }) => {
+const ProductFilterRow = ({ onViewChange, onSortChange }) => {
 
     const [isGridView, setGridView] = useState(true);
+    const [isSortMenuOpen, setSortMenuOpen] = useState(false);
     const totalProducts = useSelector((state) => state.product.total);
 
     const handleGridViewClick = () => {
@@ -15,9 +16,21 @@ const ProductFilterRow = ({ onViewChange }) => {
     };
 
     const handleListViewClick = () => {
+        
         setGridView(false);
         onViewChange("list");
     };
+
+    const toggleSortMenu = () => {
+        setSortMenuOpen((prevState) => !prevState); 
+    };
+
+    const handleSortClick = (sort) => {
+
+        onSortChange(sort);
+        setSortMenuOpen(false);
+        
+    }
 
     return (
         <div className="px-8 sm:mx-10 md:px-20 lg:px-32 xl:px-52">
@@ -34,7 +47,31 @@ const ProductFilterRow = ({ onViewChange }) => {
                         <List className="h-4" />
                     </ButtonSm>
                 </div>
-                <div className="flex-1 flex sm:justify-end">
+
+                <div className="flex-1 flex sm:justify-end gap-4">
+                    {/* Sort button and dropdown menu */}
+                    <div className="relative">
+                        <ButtonMd onClick={toggleSortMenu}>
+                            Sort
+                        </ButtonMd>
+
+                        {isSortMenuOpen && (
+                            <div className="absolute right-0 top-full mt-2 w-48 bg-white shadow-lg border rounded-md z-50">
+                                <ul className="text-sm">
+                                    <li 
+                                        onClick={() => handleSortClick("price:desc")}
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-100">Price: High to Low</li>
+                                    <li
+                                        onClick={() => handleSortClick("price:asc")}
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-100">Price: Low to High </li>
+                                    <li 
+                                        onClick={() => handleSortClick("rating:desc")}
+                                        className="px-4 py-2 cursor-pointer hover:bg-gray-100">Rating: Highest</li>
+                                </ul>
+                            </div>
+                        )}
+                    </div>
+               
                     <ButtonMd>
                         Filter
                     </ButtonMd>
