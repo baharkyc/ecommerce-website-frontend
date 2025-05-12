0e9c82f7e2d8cedcb4b44fdce9ab09ddec4634d9
baharkyc <baharokuyucu@gmail.com>
2025-05-12 22:01:22 +0300

    Add missing link prop to ShopCard components in CategoryPick

--- src/components/CategoryPick.jsx
+++ src/components/CategoryPick.jsx
@@ -5,15 +5,18 @@ const CategoryPick = () => {
         <div className="flex flex-col sm:grid sm:grid-rows-2 grid-flow-col mx-32 my-16 gap-8 sm:max-h-[600px]">
             <div className="row-span-2 h-full">
                 <ShopCard 
+                    link={"/shop"}
                     imageUrl="https://images.unsplash.com/photo-1556905055-8f358a7a47b2"
                     title="Women"
                     description="Spring'25"/>
             </div>
             <ShopCard 
+                link={"/shop"}
                 imageUrl="https://images.unsplash.com/photo-1479064555552-3ef4979f8908"
                 title="Men"
                 description="Spring'25"/>
             <ShopCard 
+                link={"/shop"}
                 imageUrl="https://images.unsplash.com/photo-1705869213986-cc8dc227fae4"
                 title="Home"
                 description=""/>
