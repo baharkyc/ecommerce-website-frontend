cce75de50b5d51d5fb628fcfbcd2c8fd63dec320
baharkyc <baharokuyucu@gmail.com>
2025-05-15 23:06:37 +0300

    Add conditional rendering for OrderSummaryBox based on cart items

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import LoginForm from "../components/forms/LoginForm";
 import OrderSummaryBox from "../components/OrderSummaryBox";
 import ShoppingCartList from "../components/ShoppingCartList";
@@ -7,13 +8,20 @@ import PageContent from "../layout/PageContent";
 
 const ShoppingCartPage = () => {
 
+  const {cart} = useSelector(state => state.shoppingCart);
+
+
+
   return (
     <div>
         <Header/>
         <PageContent>
             <div className="w-full flex flex-col items-center gap-x-4 md:flex-row md:gap-x-16 md:items-start md:justify-center p-4">
                 <ShoppingCartList/>
-                <OrderSummaryBox/>
+                {cart.length > 0 && (
+                  <OrderSummaryBox/>
+                )}
+                
             </div>
         </PageContent>
         <Footer/>
