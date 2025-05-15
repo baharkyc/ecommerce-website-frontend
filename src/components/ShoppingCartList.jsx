b27f2ac50ba1ffadcc8cd70b95cd2701248ce513
baharkyc <baharokuyucu@gmail.com>
2025-05-15 23:06:48 +0300

    Enhance ShoppingCartList with select/deselect functionality and update total display

--- src/components/ShoppingCartList.jsx
+++ src/components/ShoppingCartList.jsx
@@ -8,7 +8,9 @@ import { Trash } from "lucide-react";
 import {
   addToCart,
   decreaseItemCount,
+  deselectProducts,
   removeFromCart,
+  selectAllProducts,
   toggleChecked,
 } from "../store/actions/shoppingCartActions";
 
@@ -24,33 +26,30 @@ const ShoppingCartList = () => {
   .filter((item) => item.checked)  // Filter only checked items
   .reduce((acc, item) => acc + item.count * item.product.price, 0); 
 
-  const totalCount = cart
+  const checkedCount = cart
   .filter((item) => item.checked)
   .reduce((acc, item) => acc + item.count, 0);
+
+  const totalCount = cart.reduce((acc, item) => acc + item.count, 0);
   
   const handleChecked = (id) => {
     dispatch(toggleChecked(id));
   }
 
-  useEffect(() => {
-    console.log(cart);
-  }, cart);
-
-
   if (cart.length === 0) {
     return (
-      <div className="bg-white text-second-text-color p-4 text-center  w-full text-sm font-semibold">
+      <div className="bg-white text-second-text-color py-32 text-center  w-full text-sm font-semibold">
         Your shopping cart is empty.
       </div>
     );
   }
 
   return (
-    <div className="bg-white p-4 shadow-lg rounded-md md:w-[800px] text-sm  space-y-4">
+    <div className="bg-white p-4 srounded-md md:w-[800px] text-sm  space-y-4">
 
-      <div className="text-text-color flex flex-row justify-between">
-        <h2 className="text-lg font-semibold">My Shopping Cart</h2>
-        <h2 className="text-lg font-semibold">({totalCount} Products)</h2>
+      <div className="text-text-color text-lg font-semibold flex flex-row justify-between">
+        <h2 >My Shopping Cart</h2>
+        <h2 >{checkedCount} of {totalCount} Products Selected</h2>
       </div>
       
 
@@ -79,7 +78,7 @@ const ShoppingCartList = () => {
           <div className=" flex-1 flex flex-col items-start">
             <p className="font-bold text-text-color">{item.product.name}</p>
             <p className="font-normal text-second-text-color">{item.product.description}</p>
-            <p className="text-xs text-green-600 mt-1">Shipping tomorrow latest!</p>
+            <p className="text-xs text-green-600 mt-1">Shipment in 2-3 business days</p>
           </div>
 
           {/* Count Control */}
@@ -115,12 +114,33 @@ const ShoppingCartList = () => {
         </div>
       ))}
 
-      {/* Cart Total */}
-      <div className="flex justify-center md:justify-end gap-16 items-center pt-4 border-t font-semibold text-base">
-        <span>Total:</span>
-        <span className="text-primary-color">₺{totalPrice.toFixed(2)}</span>
-      </div>
+      <div className="grid grid-cols-2 justify-between items-center text-normal py-2 px-6">
+        <div className=" flex gap-1 md:gap-4">
+          {/* Select All */}
+          <div className=" hover:text-primary-color">
+            <button onClick={() => dispatch(selectAllProducts())}>
+              Select All
+            </button>
+          </div>
 
+          <span>|</span>
+
+          {/* Deselect All */}
+          <div className=" hover:text-primary-color">
+            <button onClick={() => dispatch(deselectProducts())}>
+              Deselect All
+            </button>
+          </div>
+        </div>
+        
+        {/* Cart Total */}
+        <div className="flex text-lg justify-center md:justify-between gap-2 md:ml-48 items-center font-semibold ">
+          <span>Total:</span>
+          <span className="text-primary-color">{totalPrice.toFixed(2)}₺</span>
+        </div>
+
+      </div>
+        
       
     </div>
   );
