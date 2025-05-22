cc025180e188de1615c78673099797c454dd9d37
baharkyc <baharokuyucu@gmail.com>
2025-05-22 23:08:18 +0300

    Refactor OrderSuccessPage for improved loading state handling and navigation

--- src/pages/OrderSuccessPage.jsx
+++ src/pages/OrderSuccessPage.jsx
@@ -3,28 +3,48 @@ import OrderSuccessBox from "../components/OrderSuccessBox";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import PageContent from "../layout/PageContent";
+import ButtonMd from "../components/buttons/ButtonMd";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Loading from "../components/Loading";  
 
 const OrderSuccessPage = () => {
 
-    const { pastOrders } = useSelector(state => state.client);
+    const { lastOrder } = useSelector(state => state.client); 
+    const { isLoading } = useSelector(state => state.global); 
+    const history = useHistory();
 
-    const lastOrder = pastOrders[pastOrders.length - 1];
+    const handleMyOrdersButtonClick = () => {
+        history.push("/previousOrders");
+    }
+
+    if (isLoading) {
+        return (
+            <div className="pt-64">
+                <Loading />
+            </div>
+            
+        ) 
+    }
 
     return (
         <div>
-        <Header/>
-        <PageContent>
-            <div className="w-full flex flex-col items-center gap-x-4 md:flex-row md:gap-x-16 md:items-start md:justify-center p-4">
-                {lastOrder ? (
-                    <OrderSuccessBox order={lastOrder} />
-                ) : (
-                    <div className="text-center text-gray-500">No order information available.</div>
-                )}
-      
-            </div>
-        </PageContent>
-        <Footer/>
-    </div>
+            <Header/>
+            <PageContent>
+                <div className="w-full flex flex-col items-center gap-y-8 gap-x-4 md:gap-x-16 md:justify-center p-4">
+                    {lastOrder ? (
+                        <OrderSuccessBox order={lastOrder} />
+                    ) : (
+                        <div className="text-center text-gray-500 md:pt-32">No order information available.</div>
+                    )}
+                    <div className="w-48 pb-32">
+                        <ButtonMd onClick={handleMyOrdersButtonClick}>
+                            My Orders
+                        </ButtonMd>
+                    </div>
+                </div>
+            </PageContent>
+            <Footer/>
+        </div>
     )
 }
 
