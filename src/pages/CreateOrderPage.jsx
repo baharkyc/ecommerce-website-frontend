4219bce6343e72f2f2fdb81927ef68a5a4d289f7
baharkyc <baharokuyucu@gmail.com>
2025-05-22 23:08:22 +0300

    Enhance CreateOrderPage to manage order eligibility based on selected address and payment details

--- src/pages/CreateOrderPage.jsx
+++ src/pages/CreateOrderPage.jsx
@@ -1,10 +1,22 @@
+import { useSelector } from "react-redux";
 import CreateOrderDetailTabs from "../components/CreateOrderDetailTabs";
 import OrderSummaryBox from "../components/OrderSummaryBox";
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import PageContent from "../layout/PageContent";
+import { useEffect, useState } from "react";
 
 const CreateOrderPage = () => {
+
+    const {selectedAddressId, selectedCardId, selectedBillingAddressId} = useSelector(state => state.client);
+    const [ canOrder, setCanOrder ] = useState(false);
+
+    useEffect(() => {
+
+        const allSelected = selectedAddressId && selectedCardId && selectedBillingAddressId; //Is all information selected to order properly
+        setCanOrder(allSelected);
+
+    }, [selectedAddressId, selectedCardId, selectedBillingAddressId]);
 
     return (
         <div>
@@ -12,7 +24,7 @@ const CreateOrderPage = () => {
         <PageContent>
             <div className="w-full flex flex-col items-center gap-x-4 md:flex-row md:gap-x-16 md:items-start md:justify-center p-4">
                 <CreateOrderDetailTabs/>
-                <OrderSummaryBox/>
+                <OrderSummaryBox canOrder={canOrder}/>
             </div>
         </PageContent>
         <Footer/>
